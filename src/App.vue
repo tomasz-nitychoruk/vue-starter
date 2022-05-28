16ba754289da8293fdfcbc7ecd432213a1b72655
tomasz-nitychoruk <tafnitychoruk@gmail.com>
2022-05-28 10:12:54 +0200

    ex2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-    <div>
-        <h1>Twój e-mail to {{ email }}</h1>
+  <div>
+    <h1>Twój e-mail to: {{ email }}</h1>
+    
+<div v-id="email.length > 0">
+  <div v-if="email.length < 10">Ale masz krótki adres!</div>
+<div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+<div v-else class="red">Twój adres e-mail jest stanowczo za długi.</div>
+</div>
 
-        <div v-if="email.length < 10">Ale masz krótki adres!</div>
-        <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-        <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+    <input type="email" v-model="email">
+    
+  </div>
 
-        <input type="email" v-model="email">
-    </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                email: ''
-            }
-        }
-    }
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  }
+}
 </script>
+
+<style>
+.red{
+  color:red;
+}
+</style>
